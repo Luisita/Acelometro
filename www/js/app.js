7230f1a85f8da567c7839e19f26f20317966631e
Luisita <luiyi_1414@hotmail.com>
2015-02-08 21:29:04 -0600

    1

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,26 @@
 
 $(document).on("deviceready", function(){
   alert("Mueve tu teléfono");
-  setInterval(function(){ navigator.accelerometer.getCurrentAcceleration(onSuccess, onError)},1000);
+  setInterval(function(){ navigator.accelerometer.getCurrentAcceleration(onSuccess, onError)},1500);
 });
 
 function onSuccess(acceleration) {
-        alert('Acceleration X: ' + acceleration.x + '\n' +
+	var x = acceleration.x;
+	var y = acceleration.y;
+	var z = acceleration.z;
+
+	if((x>-2 && x<2) && (y>4 && y<9) && (z>5 && z<9)){
+		alert('Acceleration X: ' + acceleration.x + '\n' +
               'Acceleration Y: ' + acceleration.y + '\n' +
               'Acceleration Z: ' + acceleration.z + '\n' +
               'Timestamp: '      + acceleration.timestamp + '\n');
-    }
+	}
 
-    // onError: Failed to get the acceleration
+        
+}
+
+    // onError: Fallado la aceleración
     //
     function onError() {
-        alert('onError!');
+        alert('Fallo aceleración');
     }
