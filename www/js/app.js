fae44bdfc28dc6b577a9702c43d62d2383ae15a2
Luisita <luiyi_1414@hotmail.com>
2015-02-08 20:39:36 -0600

    1

--- www/js/app.js
+++ www/js/app.js
@@ -1,23 +1,24 @@
 $(document).on("deviceready", function(){
-	$.mobile.loading( "show", {
-		text: "Esperando posición",
-		textVisible: true,
-		theme: "a",
-		html: ""
-	});
-	// Definimos una función para que compruebe la posicion del telefono cada 1 segundo
-	var acel = setInterval(function() {
-	navigator.accelerometer.getCurrentAcceleration(Aceleracion, null);
-	}, 1000);
+  $.mobile.loading( "show", {
+    text: "Esperando posición",
+    textVisible: true,
+    theme: "a",
+    html: ""
+  });
+  // Definimos una función para que compruebe la posicion del telefono cada 1 segundo
+  var acel = setInterval(function() {
+     navigator.accelerometer.getCurrentAcceleration(verificar, null);
+  }, 1000);
 });
+
 // Verificamos si se ha encontrado un punto en los valores
-function Aceleracion(acel){
-	var x = acel.x.toFixed(2);
-	var y = acel.y.toFixed(2);
-	var z = acel.z.toFixed(2);
-	if ((x >= 9 && x <= 10) && (y >= 0 && y <= 1) && (z >= 5 && z <= 6)) {
-		alert("Posición: \n x="+x+"\n y="+y+"\n z="+z);
-		navigator.notification.beep(500);
-		$.mobile.loading( "hide");
-	};
+function verificar(acel){
+  var x = acel.x.toFixed(2);
+  var y = acel.y.toFixed(2);
+  var z = acel.z.toFixed(2);
+  if ((x >= 9 && x <= 10) && (y >= 0 && y <= 1) && (z >= 5 && z <= 6)) {
+    alert("Encontraste la posición");
+    navigator.notification.beep(500);
+    $.mobile.loading( "hide");
+  };
 }
