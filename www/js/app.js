2c0f4b67b218f937db4c34bb687f69dca8f0c823
Luisita <luiyi_1414@hotmail.com>
2015-02-08 21:13:40 -0600

    1

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,19 @@
-
 $(document).on("deviceready", function(){
-  alert("Mueve tu teléfono");
-  navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+	alert("Mueve tu Teléfono");
+// Definimos una función para que compruebe la posicion del telefono cada 1 segundo
+	var acel = setInterval(function() {
+	navigator.accelerometer.getCurrentAcceleration(verificar, null);
+	}, 1000);
 });
 
-function onSuccess(acceleration) {
-        alert('Acceleration X: ' + acceleration.x + '\n' +
-              'Acceleration Y: ' + acceleration.y + '\n' +
-              'Acceleration Z: ' + acceleration.z + '\n' +
-              'Timestamp: '      + acceleration.timestamp + '\n');
-    }
-
-    // onError: Failed to get the acceleration
-    //
-    function onError() {
-        alert('onError!');
-    }
+// Verificamos si se ha encontrado un punto en los valores
+function verificar(acel){
+	var x = acel.x.toFixed(2);
+	var y = acel.y.toFixed(2);
+	var z = acel.z.toFixed(2);
+	if ((x >= 5 && x <= 10) && (y >= 0 && y <= 3) && (z >= 5 && z <= 9)) {
+		alert("Encontraste la posición");
+		navigator.notification.beep(500);
+		$.mobile.loading( "hide");
+	};
+}
